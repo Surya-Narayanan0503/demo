63641025228d12c8d1551341c4b2186d4c6a7c2c
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2022-03-03 15:21:42 -0500

    login

--- client/src/pages/router/index.ts
+++ client/src/pages/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory, RouteRecord, RouteRecordRaw } from "vue-router";
 
-import Messages from '../pages/Messages.vue';
+// import Messages from '../pages/Messages.vue';
 import Home from '../pages/Home.vue';
 import Generic from '../pages/Generic.vue';
+import Login from '../pages/Login.vue';
+import session from "../../models/session";
 
 
 // 2. Define some routes
@@ -12,9 +14,9 @@ const routes: RouteRecordRaw[] = [
   { path: '/', component: Home },
   { path: '/about', component: Generic, props: {title: 'Abut Page!'} },
   { path: '/contact', component: Generic, props: {title: 'Contact Page!'} },
-  { path: '/login', component: Generic, props: {title: 'Login Page!'} },
+  { path: '/login', component: Login },
   { path: '/signup', component: Generic, props: {title: 'SIgnUp Page!'} },
-  { path: '/messages', component: Messages },
+  { path: '/messages', component: () => import('../pages/Messages.vue') },
 ]
 
 // 3. Create the router instance and pass the `routes` option
@@ -25,6 +27,16 @@ const router = createRouter({
   history: createWebHistory(),
   routes, // short for `routes: routes`
   linkActiveClass: 'is-active',
+})
+
+router.beforeEach((to,from) =>{
+    if(['/messages'].includes(to.path))  { 
+        if(!session.user){
+            return '/login';
+
+        }
+
+    }
 })
 
 export default router;
